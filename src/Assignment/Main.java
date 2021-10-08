f2796e3c9cdcf8da66bff1772c804629b75df1c6
NIKHIL GUPTA <nikhil20530@iiitd.ac.in>
2021-10-09 00:02:56 +0530

    commited

---
+++ src/Assignment/Main.java
@@ -0,0 +1,8 @@
+package Assignment;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/Assignment/Vaccine.java
@@ -0,0 +1,4 @@
+package Assignment;
+
+public class Vaccine {
+}
