574a21392f26be7f613683a41bddb0b423035657
NIKHIL GUPTA <nikhil20530@iiitd.ac.in>
2021-11-27 13:39:50 +0530

    Q1 completed,menu completed

--- src/Assignment_4/library.java
+++ src/Assignment_4/library.java
@@ -10,15 +10,15 @@ public class library{
         List<books> book_list = new ArrayList<>();
         List<Integer> bar_list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-       /*    book_list.add(new books("A1",10,3));
-        book_list.add(new books("A1",9,3));
+          book_list.add(new books("A1",10,6));
+        book_list.add(new books("A1",9,5));
         book_list.add(new books("A2",10,3));
-        book_list.add(new books("A1",10,2));
-        book_list.add(new books("A3",10,7));
+        book_list.add(new books("A1",10,4));
         book_list.add(new books("A3",10,2));
-        */
+        book_list.add(new books("A3",10,1));
+        int slot=3;
 
-        System.out.println("WELCOME");
+       /*  System.out.println("WELCOME");
         System.out.println("Enter no of books");
         int n = sc.nextInt();
         System.out.println("Enter no of racks");
@@ -53,8 +53,7 @@ public class library{
             book_list.add(new books(name,isbn,code));
             System.out.println("BOOK ADDED!!");}
             else System.out.println("BOOK NOT ADDED");
-        }
-
+        }*/
 
         Collections.sort(book_list);
         int cur_rack=1;
@@ -68,9 +67,39 @@ public class library{
             books b1 =book_list.get(i);
             b1.setRack(cur_rack);
             b1.setSlot(cur_slot);
-            System.out.println("Title: "+b1.getTitle()+" ISBN: "+b1.getISBN()+" Barcode: "+b1.getBarcode() +" Rack number: "+b1.getRack()+" Slot number: "+b1.getSlot()  );
-            cur_slot++;
+           cur_slot++;
         }
+
+
+        int flag=1;
+
+        while (flag==1) {
+            System.out.println("CHOOSE option \n 1)Print all books \n 2)Search slot for any book \n 3)Exit");
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                for (int i = 0; i < book_list.size(); i++) {
+                    books b1 = book_list.get(i);
+                    System.out.println("Title: " + b1.getTitle() + " ISBN: " + b1.getISBN() + " Barcode: " + b1.getBarcode() + " Rack number: " + b1.getRack() + " Slot number: " + b1.getSlot());
+                }
+            } else if (choice == 2) {
+                System.out.println("Enter barcode of the book for slot");
+                int search_code = sc.nextInt();
+                for (int i = 0; i < book_list.size(); i++) {
+                    books b1 = book_list.get(i);
+                    if (b1.getBarcode() == search_code) {
+                        System.out.println("Title: " + b1.getTitle() + " ISBN: " + b1.getISBN() + " Barcode: " + b1.getBarcode() + " Rack number: " + b1.getRack() + " Slot number: " + b1.getSlot());
+                    }
+                }
+            } else if (choice == 3) {
+                System.out.println("EXITING...");
+                flag=0;
+            } else System.out.println("Enter correct value");
+        }
+
+
+
+
+
 
     }
 }
